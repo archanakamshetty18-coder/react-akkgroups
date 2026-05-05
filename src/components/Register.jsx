f39ebac1ed9071a96021aebc949a9a56f5a092c4
Archana Kamshetty <archanakamshetty18@gmail.com>
2026-05-05 16:03:17 +0530

    fixed unuse imports

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,7 @@
 import Footer from "./Footer";
 import Header from "./Header";
-import InputValMul from "./InputValMul";
-import InputValMul2 from "./InputValMul2";
+// import InputValMul from "./InputValMul";
+// import InputValMul2 from "./InputValMul2";
 import InputValMul3 from "./InputValMul3";
 
 function Register(){
--- src/components/Wish2.jsx
+++ src/components/Wish2.jsx
@@ -1,5 +1,5 @@
-import Header from "./Header"
-import Footer from "./Footer"
+// import Header from "./Header"
+// import Footer from "./Footer"
 import { useState } from "react"
 
 function Wish2(){
